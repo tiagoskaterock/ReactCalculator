ae03dfd709b37994ac8a8b8747111d27a00b1494
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-17 18:41:14 -0300

    Application done =)

--- src/calculator/components/Calculator.jsx
+++ src/calculator/components/Calculator.jsx
@@ -58,6 +58,22 @@ function reducer(state, { type, payload }) {
             }
         case ACTIONS.CLEAR:
             return {}
+        case ACTIONS.DELETE_DIGIT:
+            if(state.overwite) {
+                return {
+                    ...state,
+                    overwite: false,
+                    currentOperand: null
+                }
+            }
+            if(state.currentOperand == null) return state
+            if (state.currentOperand.length === 1) {
+                return {...state, currentOperand: null}
+            }
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1)
+            }
         case ACTIONS.EVALUATE:
             if (
                 state.operation == null || 
@@ -74,8 +90,7 @@ function reducer(state, { type, payload }) {
                 operation: null
             }
         default:
-            break;
-        
+            break;        
     }
 }
 
@@ -103,6 +118,17 @@ function evaluate({ currentOperand, previousOperand, operation }) {
     return computation.toString()
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+    maximumFractionDigits: 0,
+})
+
+function formatOperand(operand) {
+    if(operand == null) return
+    const [integer, decimal] = operand.split('.')
+    if(decimal == null) return INTEGER_FORMATTER.format(integer) 
+    return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
+
 function Calculator() {
     
     const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
@@ -110,12 +136,12 @@ function Calculator() {
     return (
         <div className="calculator-grid">
             <div className="output">
-                <div className="previous-operand">{previousOperand} {operation}</div>
-                <div className="current-operand">{currentOperand}</div>
+                <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+                <div className="current-operand">{formatOperand(currentOperand)}</div>
             </div>
 
             <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
-            <button>DEL</button>
+            <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT})}>DEL</button>
             <OperationButton operation="÷" dispatch={dispatch} />
             <DigitButton digit="1" dispatch={dispatch} />
             <DigitButton digit="2" dispatch={dispatch} />
